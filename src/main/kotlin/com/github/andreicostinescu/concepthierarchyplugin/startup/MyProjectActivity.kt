b6d1935056a06a03f72e02d7390ec023d5569cdb
GitHub Action <action@github.com>
2025-12-26 07:25:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/andreicostinescu/concepthierarchyplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/andreicostinescu/concepthierarchyplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.andreicostinescu.concepthierarchyplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/andreicostinescu/concepthierarchyplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/andreicostinescu/concepthierarchyplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.andreicostinescu.concepthierarchyplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.andreicostinescu.concepthierarchyplugin.MyBundle
+import com.github.andreicostinescu.concepthierarchyplugin.services.MyProjectService
 import javax.swing.JButton
 
 
