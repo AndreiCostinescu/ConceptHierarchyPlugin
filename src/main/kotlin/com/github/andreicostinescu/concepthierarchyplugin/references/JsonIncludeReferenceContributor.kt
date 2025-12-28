87085f1fd9979cf8b4b6240d1fab8f1e7eb08b43
Andrei Costinescu <andrei.costinescu@yahoo.com>
2025-12-28 08:57:52 +0100

    Rework JsonIncludeReferenceContributor.kt to correctly resolve header files for ValueDomains and for Functions

--- src/main/kotlin/com/github/andreicostinescu/concepthierarchyplugin/references/JsonIncludeReferenceContributor.kt
+++ src/main/kotlin/com/github/andreicostinescu/concepthierarchyplugin/references/JsonIncludeReferenceContributor.kt
@@ -1,8 +1,10 @@
 package com.github.andreicostinescu.concepthierarchyplugin.references
 
+import com.github.andreicostinescu.concepthierarchyplugin.services.ModelService
 import com.github.andreicostinescu.concepthierarchyplugin.settings.RootFileSettings
 import com.intellij.json.JsonLanguage
 import com.intellij.json.psi.JsonArray
+import com.intellij.json.psi.JsonObject
 import com.intellij.json.psi.JsonProperty
 import com.intellij.json.psi.JsonStringLiteral
 import com.intellij.openapi.components.service
@@ -10,8 +12,8 @@ import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.patterns.PlatformPatterns.psiElement
-import com.intellij.patterns.StandardPatterns.string
 import com.intellij.psi.*
+import com.intellij.psi.util.parentOfType
 import com.intellij.util.ProcessingContext
 
 class JsonIncludeReferenceContributor : PsiReferenceContributor() {
@@ -33,21 +35,49 @@ class JsonIncludeReferenceContributor : PsiReferenceContributor() {
 
                     // differentiate between include files in an array (e.g. "external") or simple string values
                     // val property = element.parent?.parent as? JsonProperty ?: return PsiReference.EMPTY_ARRAY  // works only for path-/string-values inside an array
-                    val property = com.intellij.psi.util.PsiTreeUtil.getParentOfType(path, JsonProperty::class.java)
-                        ?: return PsiReference.EMPTY_ARRAY
+                    val property = path.parentOfType<JsonProperty>() ?: return PsiReference.EMPTY_ARRAY
 
                     // check if the property name is one of the valid ones to be interpreted as an external context
                     if (!contextWhereExternalFilesAreValid.contains(property.name)) {
                         return PsiReference.EMPTY_ARRAY
                     }
+                    val log = com.intellij.openapi.diagnostic.Logger.getInstance(ModelService::class.java)
                     when (property.name) {
                         "header" -> {
-                            return arrayOf(HeaderIncludeReference(path))
+                            // "<conceptName>"-JsonProperty -> dict-JsonObject -> "data"-JsonProperty ->
+                            //  -> dict-JsonObject -> "header"-JsonProperty
+                            val dataDict = property.parent
+                            assert(dataDict is JsonObject)
+                            val dataKey = dataDict.parent
+                            assert(dataKey is JsonProperty)
+                            val conceptDict = dataKey.parent
+                            assert(conceptDict is JsonObject)
+                            val concept = conceptDict.parent
+                            assert(concept is JsonProperty)
+                            val conceptName = (concept as JsonProperty).name
+                            val model = path.project.service<ModelService>()
+                            // a Function is also a ValueDomain => test for Function first and then for ValueDomain
+                            return if (model.isFunction(conceptName)) {
+                                arrayOf(HeaderIncludeReference(path, HeaderIncludeReference.HeaderType.FunctionHeader))
+                            } else if (model.isValueDomain(conceptName)) {
+                                arrayOf(
+                                    HeaderIncludeReference(
+                                        path,
+                                        HeaderIncludeReference.HeaderType.ValueDomainHeader
+                                    )
+                                )
+                            } else {
+                                log.warn("$conceptName doesn't seem to be a concept!")
+                                PsiReference.EMPTY_ARRAY
+                            }
                         }
                         "external" -> {
                             // Optional: ensure this literal is inside an array value of the includes property
                             val isInsideArray = property.value is JsonArray
-                            if (!isInsideArray) return PsiReference.EMPTY_ARRAY
+                            if (!isInsideArray) {
+                                log.warn("External ConceptHierarchy files not bundled in an array!")
+                                return PsiReference.EMPTY_ARRAY
+                            }
                             return arrayOf(IncludeReference(path))
                         }
                         else -> {
@@ -64,26 +94,31 @@ class IncludeReference(private val lit: JsonStringLiteral) :
     PsiReferenceBase<JsonStringLiteral>(lit, true) {
 
     override fun resolve(): PsiElement? {
-        val baseDir = lit.containingFile.virtualFile.parent
-        val target = VfsUtil.findRelativeFile(lit.value, baseDir) ?: return null
-        return PsiManager.getInstance(lit.project).findFile(target)
+        val baseDir = this.lit.containingFile.virtualFile.parent
+        val target = VfsUtil.findRelativeFile(this.lit.value, baseDir) ?: return null
+        return PsiManager.getInstance(this.lit.project).findFile(target)
     }
 
     override fun getVariants(): Array<Any> = emptyArray()
 }
 
-class HeaderIncludeReference(private val lit: JsonStringLiteral) :
+class HeaderIncludeReference(private val lit: JsonStringLiteral, private val headerType: HeaderType) :
     PsiReferenceBase<JsonStringLiteral>(lit, true) {
 
+    enum class HeaderType(val dirName: String) {
+        FunctionHeader("functions"),
+        ValueDomainHeader("valueDomains")
+    }
+
     override fun resolve(): PsiElement? {
-        val project = lit.project
+        val project = this.lit.project
         val projectBase: VirtualFile? =
             project.basePath?.let { LocalFileSystem.getInstance().findFileByPath(it) }
                 ?: project.projectFile?.parent  // .ipr-based projects (if applicable)
                 ?: project.workspaceFile?.parent  // directory-based projects; optional fallback
-                ?: lit.containingFile.virtualFile.parent
+                ?: this.lit.containingFile.virtualFile.parent
         val projectName = project.name
-        val adjustedPath = join("include/$projectName", lit.value) // include/<projectName>/<value>
+        val adjustedPath = join("include/$projectName/${this.headerType.dirName}", this.lit.value) // include/<projectName>/<conceptType>/<value>
         val target = VfsUtil.findRelativeFile(adjustedPath, projectBase) ?: return null
         return PsiManager.getInstance(project).findFile(target)
     }
